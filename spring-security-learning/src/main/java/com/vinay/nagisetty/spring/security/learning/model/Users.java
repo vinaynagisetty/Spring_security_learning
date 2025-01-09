7a26b4dbdec5f4aaa53e52bcc9c865d99b38a0c8
vinay nagisetty <vinay.nagisetty@nstarxinc.com>
2025-01-09 14:32:17 +0530

    changed manual creation of getter,setter and constructor with lombok

--- spring-security-learning/src/main/java/com/vinay/nagisetty/spring/security/learning/model/Users.java
+++ spring-security-learning/src/main/java/com/vinay/nagisetty/spring/security/learning/model/Users.java
@@ -3,7 +3,13 @@ package com.vinay.nagisetty.spring.security.learning.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Users {
 
@@ -13,36 +19,36 @@ public class Users {
     private String password;
 
 
-    public Users() {
-    }
-
-    public Users(int id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+//    public Users() {
+//    }
+//
+//    public Users(int id, String username, String password) {
+//        this.id = id;
+//        this.username = username;
+//        this.password = password;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
 }
