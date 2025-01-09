3f32b09d3003ab7de86fd12c0ec9bdc9f5e25a2a
vinay nagisetty <vinay.nagisetty@nstarxinc.com>
2025-01-09 12:15:28 +0530

    generated the jwt token for evrery login request

--- spring-security-learning/src/main/java/com/vinay/nagisetty/spring/security/learning/serivce/JWTService.java
+++ spring-security-learning/src/main/java/com/vinay/nagisetty/spring/security/learning/serivce/JWTService.java
@@ -1,12 +1,59 @@
 package com.vinay.nagisetty.spring.security.learning.serivce;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static javax.crypto.Cipher.SECRET_KEY;
 
 @Service
 public class JWTService {
 
-    public String generateToken() {
-        return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+    private  String SECRET_KEY="";
 
+    public JWTService() {
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey sk = keyGen.generateKey();
+            SECRET_KEY = Base64.getEncoder().encodeToString(sk.getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+//        return Jwts.builder()
+//                .setClaims(claims)
+//                .setSubject(username)
+//                .setIssuedAt(new Date(System.currentTimeMillis()))
+//                .setExpiration(new Date(System.currentTimeMillis() +  60 * 60 * 30))
+//                .signWith(getKey())
+//                .compact();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() +  60 * 60 * 30))
+                .signWith(getKey())
+                .compact();
+
+    }
+
+    private Key getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
--- spring-security-learning/src/main/java/com/vinay/nagisetty/spring/security/learning/serivce/UserService.java
+++ spring-security-learning/src/main/java/com/vinay/nagisetty/spring/security/learning/serivce/UserService.java
@@ -35,7 +35,7 @@ public class UserService {
     public String verify(Users user) {
         Authentication authentication = authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
         if (authentication.isAuthenticated()) {
-            return jwtService.generateToken()  ;
+            return jwtService.generateToken(user.getUsername())  ;
         } else {
             return "fail";
         }
